51287ae9c2cac9f43a941c01f26eb4c609492e73
yeonjunky <yeonjunky@gmail.com>
2020-12-04 11:18:08 +0900

    .

---
+++ c/9498.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    scanf("%d", &a);
+
+    if(a < 60){
+        printf("F");
+    }
+
+    else if(a < 70){
+        printf("D");
+    }
+    else if (a < 80){
+        printf("C");
+    }
+    else if (a < 90){
+        printf("B");
+    }
+    else{
+        printf("A");
+    }
+
+    return 0;
+}
