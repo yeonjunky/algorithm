a13e8a01d3f5808dfd8860bba4bee0f1d70dbe78
yeonjun <yeonjunky@gmail.com>
2022-04-14 15:18:04 +0900

    4.14

---
+++ python/1149.py
@@ -0,0 +1,20 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+costs = []
+dp = [[0, 0, 0] for _ in range(n)]
+
+for _ in range(n):
+    cost = list(map(int, sys.stdin.readline().split()))
+    costs.append(cost)
+
+dp[0][0] = costs[0][0]
+dp[0][1] = costs[0][1]
+dp[0][2] = costs[0][2]
+
+for i in range(1, n):
+    dp[i][0] = costs[i][0] + min(dp[i - 1][1], dp[i - 1][2])
+    dp[i][1] = costs[i][1] + min(dp[i - 1][0], dp[i - 1][2])
+    dp[i][2] = costs[i][2] + min(dp[i - 1][0], dp[i - 1][1])
+
+print(min(dp[n - 1]))
