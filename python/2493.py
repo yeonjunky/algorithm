082de986ad28b235a1504a37c054ea60c0cacfb6
yeonjun <yeonjunky@gmail.com>
2022-05-12 12:21:56 +0900

    5.12 재채점

--- python/2493.py
+++ python/2493.py
@@ -4,23 +4,22 @@ n = int(sys.stdin.readline().replace('\n', ''))
 
 towers = list(map(int, sys.stdin.readline().split(' ')))
 
-stack = []
-result = []
+stack = [] # 2D list that contains index and length
 
 for i, l in enumerate(towers):
     if len(stack) == 0:
         stack.append([i, l])
-        print(0)
+        print(0, end=' ')
 
     else:
         while stack[-1][1] < l:
             stack.pop()
 
             if len(stack) == 0:
-                print(0)
+                print(0, end=' ')
                 break
-        
+
         if len(stack) != 0:
-            print(stack[-1][0] + 1)
+            print(stack[-1][0] + 1, end=' ')
 
     stack.append([i, l])
