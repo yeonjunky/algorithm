38211eca6daa17ade22472ed03cfbd677c9871e2
yeonjun <yeonjunky@gmail.com>
2022-09-17 21:17:20 +0900

    .

--- python/1922.py
+++ python/1922.py
@@ -4,7 +4,6 @@ import sys
 N = int(sys.stdin.readline().rstrip())
 M = int(sys.stdin.readline().rstrip())
 parent = [i for i in range(N + 1)]
-visited = [0] * N
 edges = []
 cost = 0
 edge_cnt = 0
@@ -28,14 +27,13 @@ def is_cycle(node1, node2):
     return find_root(node1) == find_root(node2)
 
 
-
 for _ in range(M):
     edges.append(list(map(int, sys.stdin.readline().split())))
 
 edges.sort(key= lambda e : e[2])
 
 for e in edges:
-    if find_root(e[0]) != find_root(e[1]):
+    if not is_cycle(e[0], e[1]):
         cost += e[2]
         edge_cnt += 1
         union(e[0], e[1])
