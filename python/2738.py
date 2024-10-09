99d81812c36cb262f8244cba4cc04943a56d1ec3
Yeonjun Kim <yeonjuki@c2r3s8.42gyeongsan.kr>
2024-10-09 11:19:29 +0900

    2738

---
+++ python/2738.py
@@ -0,0 +1,15 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+a = []
+b = []
+
+for i in range(n):
+	a.append(list(map(int, sys.stdin.readline().split())))
+for i in range(n):
+	b.append(list(map(int, sys.stdin.readline().split())))
+
+for i in range(n):
+	for j in range(m):
+		print(a[i][j] + b[i][j], end=" ")
+	print()
