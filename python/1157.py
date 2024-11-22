b6fc42c0399f2607fe14633fac572e54db356c4b
yeonjunky <yeonjunky@gmail.com>
2024-11-22 12:11:57 +0900

    11.22 1157.py

--- python/1157.py
+++ python/1157.py
@@ -1,12 +1,9 @@
-import sys
-
 chars = {}
-word = sys.stdin.readline().replace('\n', '').upper()
+word = input().upper()
 
 for i in word:
     if not i in chars:
         chars[i] = 1
-    
     else:
         chars[i] += 1
 
@@ -14,7 +11,7 @@ nums = list(chars.values())
 copy = nums.copy()
 nums.sort()
 
-if len(word) != 1 and nums[-1] == nums[-2]:
-    print("?")
-else:
+if len(nums) == 1 or (nums[-1] > nums[-2]):
     print(list(chars.keys())[copy.index(nums[-1])])
+else:
+    print("?")
