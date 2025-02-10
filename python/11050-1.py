46b9a5436273e2c8b249153e26446548dd3e8b7f
yeonjunky <yeonjunky@gmail.com>
2025-02-10 13:00:22 +0900

    2.10

---
+++ python/11050-1.py
@@ -0,0 +1,12 @@
+n, k = map(int, input().split())
+
+dp = [[0] * (k + 1) for _ in range(n + 1)]
+
+for i in range(n + 1):
+    for j in range(min(k + 1, i + 1)):
+        if j == 0 or i == j:
+            dp[i][j] = 1
+        else:
+            dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1]
+
+print(dp[n][k])
